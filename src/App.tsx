35bd79a0ed16cb22fa918a66d801ab87cf299e69
Alekmz <amalexmuniz@gmail.com>
2023-01-17 22:37:50 -0300

    feat: initial development frontend nlw setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './styles/global.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+     <p>Hello NLW</p>
+    </div>
+  )
+}
+
+export default App
