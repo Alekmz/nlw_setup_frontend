44e07a21537d0af19a3e4446be4b86294e693b29
Alekmz <amalexmuniz@gmail.com>
2023-01-21 15:28:11 -0300

    feat: finish second class

---
+++ src/utils/generate-dates-range.ts
@@ -0,0 +1,15 @@
+import dayjs from "dayjs";
+export function generateDatesRange() {
+  const firstDayOfTheYear = dayjs().startOf("year");
+  const today = new Date();
+
+  const dates: Date[] = [];
+  let compareDate = firstDayOfTheYear;
+
+  while (compareDate.isBefore(today)) {
+    dates.push(compareDate.toDate());
+    compareDate = compareDate.add(1, "day");
+  }
+
+  return dates;
+}
